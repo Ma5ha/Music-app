0dcba3db321b4da5164a599ef1843b7e2ba086e2
Mladen <blagmash@gmail.com>
2019-12-24 14:55:30 +0100

    Home page added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'front_pages/Home'
+  root 'front_pages#Home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
