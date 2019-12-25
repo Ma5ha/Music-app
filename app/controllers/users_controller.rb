6c64a782f355046c78d96fef87d1525b51cfe7dd
Mladen <blagmash@gmail.com>
2019-12-25 14:31:19 +0100

    +users_controller_create,show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,27 @@
 class UsersController < ApplicationController
+  include UsersHelper
   def new
     @user = User.new
   end
 
   def show
+    @user=User.find_by(id: params[:id])
   end
   def create
+    user=User.new(strong_params)
+    if user.save!
+      login(user)
+      render "show"
+      flash.now[:message] = "Welcome #{user.name} to music app!"
+    else
+      render "new"
+    end
   end
+
+
+private
+def strong_params
+  params.require(:user).permit(:name,:email,:password,:password_confirmation)
+end
+
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,6 @@
 module UsersHelper
+    
+    def login(user)
+        session[:user_id] = user.id
+      end
 end
