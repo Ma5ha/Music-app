a9386ee12304af5c169e583c37d206dc4c84e30a
Mladen <blagmash@gmail.com>
2019-12-26 10:11:11 +0100

    +uniquness to usesr email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
+  include UsersHelper
   def new
+    @user=User.new
+  end
+  def create
+    @user=User.find_by(email:params[:user][:email], password:params[:user][:password])
+    if @user.name && user.authenticate(params[:password])
+      login(@user)
+      redirect_to controller: "users", action: 'show'
+    else 
+      flash[:danger] = "Wrong email/password, please try again, or sign up if u have profile!"
+      render "new"
+    end
   end
 end
+
