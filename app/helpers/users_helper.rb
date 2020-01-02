13fadc80d996596bb700226af2c688819b9b68f2
Mladen <blagmash@gmail.com>
2020-01-02 14:14:31 +0100

    +users show refactored

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,12 +3,15 @@ module UsersHelper
     def login(user)
         session[:user_id] = user.id
       end
-      def gravatar_for(user)
+      def gravatar_for(user, size: 200)
         gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
         image_tag(gravatar_url, alt: user.name, class: "gravatar")
         
+
       end
+      
+      
       def remeber(user)
         user.remeber
         coockies.permanent.signed[:user_id]=user.id
